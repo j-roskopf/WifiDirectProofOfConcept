5682c42d21fa2471d49a21510a30ede2db2e6897
Joe Roskopf <joseph.thomas.roskopf@gmail.com>
2023-03-28 21:54:25 -0500

    Initial commit

---
+++ app/src/main/java/com/joetr/stackoverflowwifidirect/WiFiP2pService.java
@@ -0,0 +1,12 @@
+package com.joetr.stackoverflowwifidirect;
+
+
+import android.net.wifi.p2p.WifiP2pDevice;
+/**
+ * A structure to hold service information.
+ */
+public class WiFiP2pService {
+    WifiP2pDevice device;
+    String instanceName = null;
+    String serviceRegistrationType = null;
+}
